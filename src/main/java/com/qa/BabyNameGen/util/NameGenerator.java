8d84c8c4d897148948d7b0cd765da3ba64c7f612
alvinjo <alvin.joseph0@gmail.com>
2018-12-04 15:06:21 +0000

    something

--- src/main/java/com/qa/BabyNameGen/util/NameGenerator.java
+++ src/main/java/com/qa/BabyNameGen/util/NameGenerator.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 public class NameGenerator {
 
+    private static char[] cons = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+    private static char[] vow = {'a','e','i','o','u'};
+    private static boolean consVowel = true;
 
     public static String generateString(int length, String startsWith){
         if(lengthIsSameOrExceeds(length, startsWith.length())){
@@ -22,8 +25,13 @@ public class NameGenerator {
         return requiredLength<=stringLength;
     }
 
-    private static String makeCharacter(){
-        int letterAscii = new Random().nextInt(25)+97;
-        return Character.toString((char)letterAscii);
+    private static char makeCharacter(){
+        consVowel = !consVowel;
+        if(consVowel){
+            return cons[new Random().nextInt(cons.length)];
+        }else{
+            return vow[new Random().nextInt(vow.length)];
+        }
     }
+
 }
